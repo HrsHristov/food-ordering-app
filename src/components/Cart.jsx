0c9a43f0248ff71591d538655f74536c63d91d99
Hristo Hristov <hrshristov@gmail.com>
2024-01-30 14:59:39 +0200

    Add functionality to close the cart modal

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,16 +3,22 @@ import Modal from "./UI/Modal";
 import CartContext from "../store/CartContex";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
 
 const Cart = () => {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
 
     const cartTotal = cartCtx.items.reduce(
         (totalPrice, item) => totalPrice + item.quantity * item.price,
         0
     );
+
+    const handleCloseCart = () => {
+        userProgressCtx.hideCart();
+    };
     return (
-        <Modal className="cart">
+        <Modal className="cart" open={userProgressCtx.progress === "cart"}>
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map((item) => (
@@ -24,8 +30,10 @@ const Cart = () => {
 
             <p className="total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
-                <Button textOnly>Close</Button>
-                <Button textOnly>Go to Checkout</Button>
+                <Button textOnly onClick={handleCloseCart}>
+                    Close
+                </Button>
+                <Button onClick={handleCloseCart}>Go to Checkout</Button>
             </p>
         </Modal>
     );
