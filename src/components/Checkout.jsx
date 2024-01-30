06fd5d309faa00fd593fe59a6753168f8f865a07
Hristo Hristov <hrshristov@gmail.com>
2024-01-30 23:16:19 +0200

    implement useHttp hook

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -7,10 +7,27 @@ import Modal from "./UI/Modal";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import useHttp from "../hooks/useHttp";
+import Error from "./Error";
+
+const requestConfig = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+};
 
 const Checkout = () => {
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext);
+
+    const {
+        data,
+        isLoading: isSending,
+        error,
+        sendRequest,
+        clearData,
+    } = useHttp("http://localhost:3000/orders", requestConfig);
 
     const cartTotal = cartCtx.items.reduce(
         (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -21,27 +38,79 @@ const Checkout = () => {
         userProgressCtx.hideCheckOut();
     };
 
+    const handleFinish = () => {
+        userProgressCtx.hideCheckOut();
+        cartCtx.clearCart();
+        clearData();
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const fd = new FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries()); //{email: example@gmail.com}
+
+        sendRequest(
+            JSON.stringify({
+                order: {
+                    items: cartCtx.items,
+                    customer: customerData,
+                },
+            })
+        );
+    };
+
+    let actions = (
+        <>
+            <Button type="button" textOnly onClick={handleClose}>
+                Close
+            </Button>
+            <Button>Submit Order</Button>
+        </>
+    );
+
+    if (isSending) {
+        actions = <span>Sending order data...</span>;
+    }
+
+    if (data && !error) {
+        return (
+            <Modal
+                open={userProgressCtx.progress === "checkout"}
+                onClose={handleFinish}
+            >
+                <h2>Success!</h2>
+                <p>Your order was submitted successfully.</p>
+                <p>
+                    We will get back to you with more detials within the next
+                    few minutes
+                </p>
+                <p className="modal-actions">
+                    <Button onClick={handleFinish}>Ok</Button>
+                </p>
+            </Modal>
+        );
+    }
+
     return (
         <Modal
             open={userProgressCtx.progress === "checkout"}
             onClose={handleClose}
         >
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-                <Input label="Full Name" type="text" id="full-name" />
+                <Input label="Full Name" type="text" id="name" />
                 <Input label="Email Address" type="email" id="email" />
                 <Input label="Street" type="text" id="street" />
                 <div className="control-row">
                     <Input label="Postal Code" type="text" id="postal-code" />
                     <Input label="City" type="text" id="city" />
                 </div>
-                <p className="modal-actions">
-                    <Button type="button" textOnly onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button>Submit Order</Button>
-                </p>
+                {error && (
+                    <Error title="Failed to submit order" message={error} />
+                )}
+                <p className="modal-actions">{actions}</p>
             </form>
         </Modal>
     );
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
+import Error from "./Error";
+
+const requestConfig = {};
 
 const Meals = () => {
-    const [loadedMeals, setLoadedMeals] = useState([]);
+    const {
+        data: loadedMeals,
+        isLoading,
+        error,
+    } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-    useEffect(() => {
-        const fetchMeals = async () => {
-            const response = await fetch("http://localhost:3000/meals");
+    if (isLoading) {
+        return <p className="center">Fetching meals...</p>;
+    }
 
-            if (!response.ok) {
-                //...
-            }
-
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        };
-
-        fetchMeals();
-    }, []);
+    if (error) {
+        return <Error title="Failed to fetch meals" message={error} />;
+    }
 
     return (
         <ul id="meals">
             {loadedMeals.map((meal) => (
-                <MealItem meal={meal} />
+                <MealItem key={meal.id} meal={meal} />
             ))}
         </ul>
     );
