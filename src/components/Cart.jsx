ecd22e407c7cb4ca7989bda7f1cd4fd32633477b
Hristo Hristov <hrshristov@gmail.com>
2024-01-30 20:48:44 +0200

    handle open/close to the modals

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,9 @@
 import { useContext } from "react";
+
+import { currencyFormatter } from "../util/formatting";
+
 import Modal from "./UI/Modal";
 import CartContext from "../store/CartContex";
-import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
 import CartItem from "./CartItem";
@@ -18,8 +20,19 @@ const Cart = () => {
     const handleCloseCart = () => {
         userProgressCtx.hideCart();
     };
+
+    const handleGoToCheckout = () => {
+        userProgressCtx.showCheckout();
+    };
+
     return (
-        <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+        <Modal
+            className="cart"
+            open={userProgressCtx.progress === "cart"}
+            onClose={
+                userProgressCtx.progress === "cart" ? handleCloseCart : null
+            }
+        >
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map((item) => (
@@ -41,7 +54,7 @@ const Cart = () => {
                     Close
                 </Button>
                 {cartCtx.items.length > 0 && (
-                    <Button onClick={handleCloseCart}>Go to Checkout</Button>
+                    <Button onClick={handleGoToCheckout}>Go to Checkout</Button>
                 )}
             </p>
         </Modal>
